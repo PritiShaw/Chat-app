2d3146ef87e066376e99553feefe6902c655da1e
PritiShaw <pritishaw0103@gmail.com>
2020-11-03 02:52:01 +0530

    Add time

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ io.on('connection', socket => {
   const id = socket.handshake.query.id
   socket.join(id)
 
-  socket.on('send-message', ({ recipients, text }) => {
+  socket.on('send-message', ({ recipients, text,time }) => {
     recipients.forEach(recipient => {
+      console.log(time, id , text)
       const newRecipients = recipients.filter(r => r !== recipient)
       newRecipients.push(id)
       socket.broadcast.to(recipient).emit('receive-message', {
