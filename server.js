0f73b3b854d8314b41a4c2cbef325aba9aa56648
PritiShaw <pritishaw0103@gmail.com>
2020-11-03 17:49:23 +0530

    handle Broadcast

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ io.on('connection', socket => {
   const id = socket.handshake.query.id
   socket.join(id)
 
-  socket.on('send-message', ({ recipients, text,time }) => {
-    recipients.forEach(recipient => {
-      console.log(time, id , text)
-      const newRecipients = recipients.filter(r => r !== recipient)
-      newRecipients.push(id)
-      socket.broadcast.to(recipient).emit('receive-message', {
-        recipients: newRecipients, sender: id, text, time
+  socket.on('send-message', ({ recipients, text, time }) => {
+    if (recipients.length == 1 && recipients[0] == "BROADCAST") {
+      socket.broadcast.emit('receive-message', {
+        recipients, sender: id, text, time
       })
-    })
+    }
+    else
+      recipients.forEach(recipient => {
+        console.log(time, id, text)
+        const newRecipients = recipients.filter(r => r !== recipient)
+        newRecipients.push(id)
+        socket.broadcast.to(recipient).emit('receive-message', {
+          recipients: newRecipients, sender: id, text, time
+        })
+      })
   })
 })
